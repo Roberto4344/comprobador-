fbdd036b9d2e9878b7f068522ea7a0b7333dd09f
Roberto <robertoabad93@gmail.com>
2021-01-11 10:20:09 +0100

    01 se copia el test y se hace compilar

---
+++ ComprobadorDeMultiplos.java
@@ -0,0 +1,28 @@
+public class ComprobadorDeMultiplos{
+
+    public ComprobadorDeMultiplos(){
+    }
+
+    public boolean esMultiploDe2(int comproba1){
+        if (comproba1 % 2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean esMultiploDe5(int comproba1){
+         if (comproba1 % 5 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean esMultiploDe(int comproba1,int multiplo){
+        return true;
+    }
+
+}
