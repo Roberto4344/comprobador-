da70dbc27b7289215005a13fafd9d6abf0b82e13
Roberto <robertoabad93@gmail.com>
2021-01-12 09:26:06 +0100

    02.2 se corrigen fallos para que funcione perfectamente el test

--- ComprobadorDeMultiplos.java
+++ ComprobadorDeMultiplos.java
@@ -4,30 +4,45 @@ public class ComprobadorDeMultiplos{
     }
 
     public boolean esMultiploDe2(int comproba1){
-        if (comproba1 % 2 == 0){
-            return true;
-        }
-        else {
+        if (comproba1 == 0){
             return false;
+        }
+        else{
+            if (comproba1 % 2 == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 
     public boolean esMultiploDe5(int comproba1){
-        if (comproba1 % 5 == 0){
-            return true;
-        }
-        else {
+        if(comproba1 <= 0 ){
             return false;
+        }
+        else{
+            if (comproba1 % 5 == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 
     public boolean esMultiploDe(int comproba1,int multiplo){
-        if (comproba1 % multiplo == 0){
-            return true;
-        }
-        else {
+        if(comproba1 < multiplo || comproba1 == 0 || multiplo == 0){
             return false;
         }
+        else{
+            if (comproba1 % multiplo == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
 
+        }
     }
 }
